9e8b230c7c9d0caf5d11f0135a0db84e79293faa
Sai Sridhar PAGADALA <62647460+spnizen@users.noreply.github.com>
2023-09-22 19:21:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,95 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import base64
+import json
+from flask import Flask, render_template, request
+from worker import openai_process_message
+from flask_cors import CORS
+import os
 
-"""
-# Welcome to Streamlit!
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from ibm_watson import SpeechToTextV1, TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Using get() method to get the value from dictionary
+course={'result_index': 0, 'results': [{'final': True, 'alternatives': [{'transcript': 'hello how are you doing today ', 'confidence': 0.97}]}]}
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+print('language:', course.get('results'))
+# language: python
 
-    points_per_turn = total_points / num_turns
+print('fee:',course.get('fee'))
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+@app.route('/process-message', methods=['POST'])
+def process_message_route():
+
+    user_message = request.json['userMessage'] # Get user's message from their request
+    print('user_message', user_message)
+
+    # voice = request.json['voice'] # Get user's preferred voice from their request
+    # print('voice in process-message', voice)
+
+    # Call openai_process_message function to process the user's message and get a response back
+    openai_response_text = openai_process_message(user_message)
+
+    # Clean the response to remove any emptylines
+    openai_response_text = os.linesep.join([s for s in openai_response_text.splitlines() if s])
+    # print('<<<<<<<<<<<<<< in process-message >>>>>>>>>>>>>> ',openai_response_text)
+    # Call our text_to_speech function to convert OpenAI Api's reponse to speech
+    # openai_response_speech = text_to_speech(openai_response_text, voice)
+    # print('openai_response_speech >>>>>>>>>  ',openai_response_speech)
+    # convert openai_response_speech to base64 string so it can be sent back in the JSON response
+    # openai_response_speech = base64.b64encode(openai_response_speech).decode('utf-8')
+
+    # Send a JSON response back to the user containing their message's response both in text and speech formats
+    # response = app.response_class(
+    response=json.dumps({"openaiResponseText": openai_response_text})
+        # status=200,
+        # mimetype='application/json'
+    # )
+
+    print('from prossess-message >>>>> ',response)
+    return response
+
+@app.route('/process-avatar', methods=['POST'])
+def process_avatar_route():
+
+    user_message = request.json['userMessage'] # Get user's message from their request
+    print('user_message', user_message)
+
+    # voice = request.json['voice'] # Get user's preferred voice from their request
+    # print('voice in process-message', voice)
+
+    # Call did_process_avatar function to process the user's message and get a response back
+    did_response_text = did_process_avatar(user_message)
+
+    # Clean the response to remove any emptylines
+    # openai_response_text = os.linesep.join([s for s in openai_response_text.splitlines() if s])
+    # print('<<<<<<<<<<<<<< in process-message >>>>>>>>>>>>>> ',openai_response_text)
+    # Call our text_to_speech function to convert OpenAI Api's reponse to speech
+    # openai_response_speech = text_to_speech(openai_response_text, voice)
+    # print('openai_response_speech >>>>>>>>>  ',openai_response_speech)
+    # convert openai_response_speech to base64 string so it can be sent back in the JSON response
+    # openai_response_speech = base64.b64encode(openai_response_speech).decode('utf-8')
+
+    # Send a JSON response back to the user containing their message's response both in text and speech formats
+    # response = app.response_class(
+    response=json.dumps({"openaiResponseText": openai_response_text})
+        # status=200,
+        # mimetype='application/json'
+    # )
+
+    print('from prossess-avatar >>>>> ',response)
+    return response
+
+
+
+if __name__ == "__main__":
+    # app.run(port=8000, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+
+
